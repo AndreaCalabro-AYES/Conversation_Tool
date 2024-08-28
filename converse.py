6cb18284270ad5ed8162f5cc2538b4e906a166f5
AndreaCalabro-AYES <andrea.calabro@ayesconsulting.com>
2024-08-28 09:38:11 +0200

    force it before vosk importing

--- converse.py
+++ converse.py
@@ -1,4 +1,11 @@
 import sounddevice as sd
+
+# Print the LD_LIBRARY_PATH to verify it's correctly set
+print("LD_LIBRARY_PATH:", os.environ.get('LD_LIBRARY_PATH'), flush=True)
+
+# Explicitly load the libvosk.so shared library
+ctypes.cdll.LoadLibrary('/usr/local/lib/python3.9/site-packages/vosk/libvosk.so')
+
 import vosk
 import numpy as np
 from gpiozero import LED
@@ -9,11 +16,7 @@ import os
 # Initialize the GPIO control
 led = LED(17)  # GPIO pin number
 
-# Print the LD_LIBRARY_PATH to verify it's correctly set
-print("LD_LIBRARY_PATH:", os.environ.get('LD_LIBRARY_PATH'), flush=True)
 
-# Explicitly load the libvosk.so shared library
-ctypes.cdll.LoadLibrary('/usr/local/lib/python3.9/site-packages/vosk/libvosk.so')
 
 # Load the Vosk model
 model = vosk.Model("/app/vosk-model")  # Ensure the model is correctly located in /app/vosk-model
